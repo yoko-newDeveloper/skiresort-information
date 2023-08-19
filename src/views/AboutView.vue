805d8c4e56e402cf46b73e863557faaa2c53e81b
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-08-20 05:55:30 +0900

    入力フォームの実装

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>
+    <el-radio-group v-model="labelPosition" size="small">
+      <el-radio-button label="left">Left</el-radio-button>
+      <el-radio-button label="right">Right</el-radio-button>
+      <el-radio-button label="top">Top</el-radio-button>
+    </el-radio-group>
+    <div style="margin: 20px;"></div>
+    <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign">
+      <el-form-item label="SkiresortName">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="Area">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-form-item label="Access">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+      <el-form-item label="numberOfStars">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+      <el-form-item label="RecommendedPoint">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+    </el-form>
+      <el-button type="primary" @click="onSubmit">Submit</el-button>
+      <el-button type="default" @click="onSubmit">Cancel</el-button>
+</div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      labelPosition: 'right',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="skiresortName"
+      label="SkiresortName"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="area"
+      label="Area"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="access"
+      label="Access"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="numberOfStars"
+      label="NumberOfStars"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="recommendedPoint"
+      label="RecommendedPoint"
+      width="180">
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      skiresorts: []
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      console.log(this.skiresorts)
+    }
   }
 }
 </script>
